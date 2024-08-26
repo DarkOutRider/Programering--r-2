289eaad2296eb3bed5a82864c3e2418593643139
DarkOutRider <ludvig.s.hellstrom@gmail.com>
2024-08-26 14:23:20 +0200

    uppgift 1

---
+++ Modul0.py
@@ -0,0 +1,10 @@
+print("hello world") #Hej
+name = "Ludvig"
+print(name)
+my_age = 17
+print(my_age)
+print(name + " is " + str(my_age) + " years old ")
+print("=" * 20)
+name = input("skriv namn: ")
+
+print("välkommen " + name + "\"")
---
+++ Modul1.py
@@ -0,0 +1,28 @@
+print("hello world")
+print("här är ett citat: \"jag är glad\"")
+print("lorem ipsum")
+print("""lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum
+      lorem ipsum""")
+print("Ludvig\nHellström")
+
+print("Hej kompis hej på dig")
+
+print("hello world") #Hej
+name = "Ludvig"
+print(name)
+my_age = 17
+print(my_age)
+print(name + " is " + str(my_age) + " years old ")
+print("=" * 20)
+name = input("skriv namn: ")
+
+print("välkommen " + name + "\"")
